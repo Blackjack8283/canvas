a820b255eb4770205ab1e7f37e92f7f14a60147f
Blackjack8283 <d.ito0538@gmail.com>
2021-10-19 15:58:18 +0900

    10/19 文字が書けるようになった

--- main.js
+++ main.js
@@ -2,22 +2,31 @@ const wrapper = document.getElementById("wrapper");
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
+let p_vec = [];
+
 let hold = false;
 canvas.addEventListener("pointerdown", (e)=>{
     const x = e.clientX - canvas.offsetLeft;
     const y = e.clientY - canvas.offsetTop;
     hold = true;
-    // line(x,y,r);
+    //p_vecにクリックの位置ベクトルを前から追加
+    p_vec.unshift([]);
+    p_vec[0].unshift( new Vector2(x,y) );
+    dot(p_vec[0][0]);
 });
 
 canvas.addEventListener("pointermove", (e)=>{
+    if(!hold) return;
     const x = e.clientX - canvas.offsetLeft;
     const y = e.clientY - canvas.offsetTop;
-    // line(x,y,r);
+    //移動後のマウスの位置ベクトルを前から追加
+    p_vec[0].unshift( new Vector2(x,y) );
+    line(p_vec[0][1], p_vec[0][0]);
 });
 
 canvas.addEventListener("pointerup", (e)=>{
     hold = false;
+    console.log(p_vec);
 });
 
 const r = 10;
@@ -25,38 +34,33 @@ ctx.fillStyle = "black";
 ctx.strokeStyle = "black";
 onResize();
 
-//四角形+半円二つ作って
-const a = new Vector2(300,500);
-const b = new Vector2(200,200);
-const ab = b.clone().sub(a);
-const n = ab.clone().normal().multiplyScalar(r);
-//長方形
-ctx.beginPath();
-const s = a.clone().add(n);
-const t = s.clone().add(ab);
-const v = a.clone().sub(n);
-const u = v.clone().add(ab); 
-ctx.moveTo(s.x, s.y);
-ctx.lineTo(t.x, t.y);
-ctx.lineTo(u.x, u.y);
-ctx.lineTo(v.x, v.y);
-ctx.lineTo(s.x, s.y);
-ctx.fill();
-//円二つ
-ctx.beginPath();
-ctx.arc(a.x, a.y, r, 0, 2*Math.PI, false);
-ctx.fill();
-ctx.beginPath();
-ctx.arc(b.x, b.y, r, 0, 2*Math.PI, false);
-ctx.fill();
-
-
-
-
-function line(x,y,r){
-    if(!hold) return;
+function line(a, b){ //a,b -> vector2
+    const ab = b.clone().sub(a);
+    const n = ab.clone().normal().multiplyScalar(r);
+    //長方形
     ctx.beginPath();
-    ctx.arc(x-r, y-r, r, 0, 2*Math.PI, false);
+    const s = a.clone().add(n);
+    const t = s.clone().add(ab);
+    const v = a.clone().sub(n);
+    const u = v.clone().add(ab); 
+    ctx.moveTo(s.x, s.y);
+    ctx.lineTo(t.x, t.y);
+    ctx.lineTo(u.x, u.y);
+    ctx.lineTo(v.x, v.y);
+    ctx.lineTo(s.x, s.y);
+    ctx.fill();
+    //円二つ
+    ctx.beginPath();
+    ctx.arc(a.x, a.y, r, 0, 2*Math.PI, false);
+    ctx.fill();
+    ctx.beginPath();
+    ctx.arc(b.x, b.y, r, 0, 2*Math.PI, false);
+    ctx.fill();
+}
+
+function dot(v){ // v -> vector2
+    ctx.beginPath();
+    ctx.arc(v.x, v.y, r, 0, 2*Math.PI, false);
     ctx.fill();
 }
 
