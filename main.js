355044f3e978c352955bd799cc16b1fa558453ca
Blackjack8283 <d.ito0538@gmail.com>
2021-10-20 16:03:54 +0900

    10/20 太さ変更対応

--- main.js
+++ main.js
@@ -2,12 +2,14 @@ const wrapper = document.getElementById("wrapper");
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
-//書いている時のマウスの位置情報を記録
-//色の情報を対応させる！！
+//書いている時のマウスの位置情報(２次)
 let p_vec = [];
-let colors = [];
-
+//線の色・太さの情報
+let setting = [];
+//マウスが押し込まれているかどうか
 let hold = false;
+
+//クリック時
 canvas.addEventListener("pointerdown", (e)=>{
     const x = e.clientX - canvas.offsetLeft;
     const y = e.clientY - canvas.offsetTop;
@@ -16,29 +18,35 @@ canvas.addEventListener("pointerdown", (e)=>{
     //より未来の履歴が入っている配列を消してから先頭に追加
     p_vec.splice(0,cur, []);
     //色情報追加
-    colors.splice(0,cur, pencil_color);
+    setting.splice(0,cur, {c:pencil_color, r: curr});
     cur = 0;
     //空配列ににクリックの位置ベクトルを前から追加
     p_vec[0].unshift( new Vector2(x,y) );
     color(pencil_color);
-    dot(p_vec[0][0]);
+    line(p_vec[0][0], p_vec[0][0], curr);
 });
 
+//ポインターが動いた時
 canvas.addEventListener("pointermove", (e)=>{
     if(!hold) return;
     const x = e.clientX - canvas.offsetLeft;
     const y = e.clientY - canvas.offsetTop;
     //マウスの位置ベクトルを前から追加
     p_vec[0].unshift( new Vector2(x,y) );
-    line(p_vec[0][1], p_vec[0][0]);
+    line(p_vec[0][1], p_vec[0][0], curr);
 });
 
+//マウスを離した時
 canvas.addEventListener("pointerup", (e)=>{
     hold = false;
 });
 
+//現在見ているindex
 let cur = 0;
+//現在の色
 let pencil_color = 1;
+
+//キー操作
 window.addEventListener("keydown", (e)=>{
     switch(e.key){
         case "r":
@@ -52,17 +60,39 @@ window.addEventListener("keydown", (e)=>{
             if(cur > 0) cur--;
             repaint();
             break;
+        case "d":
+            pencil_color = "d";
+            //太くする
+            curr = 12;
+            break;
+        case "a":
+            if(curr > 1) curr--;
+            break;
+        case "s":
+            curr++;
+            break;
     }
-    if(e.key.match("[0-9]")) pencil_color = Number(e.key);
+    //0~9の数字
+    if(e.key.match("[0-9]")){
+        //前の作業が削除だった場合元の太さに戻す
+        if(pencil_color == "d") curr = 8;
+        pencil_color = Number(e.key);
+    }
 });
 
-const r = 8;
+//初期化
+
+//現在のペンの太さ
+let curr = 8;
+//初期設定でペンを黒に
 color(1);
+//canvasの大きさ定める
 onResize();
 
-function line(a, b){ //a,b -> vector2
-    const ab = b.clone().sub(a);
-    const n = ab.clone().normal().multiplyScalar(r);
+//線
+function line(a, b, r){ //a,b -> vector2
+    const ab = b.clone().sub(a); //vAB 
+    const n = ab.clone().normal().multiplyScalar(r); //vABの法線ベクトル(の一つ)
     //長方形
     ctx.beginPath();
     const s = a.clone().add(n);
@@ -75,36 +105,34 @@ function line(a, b){ //a,b -> vector2
     ctx.lineTo(v.x, v.y);
     ctx.lineTo(s.x, s.y);
     ctx.fill();
-    //円二つ
+    //円１
     ctx.beginPath();
     ctx.arc(a.x, a.y, r, 0, 2*Math.PI, false);
     ctx.fill();
+    //円２
     ctx.beginPath();
     ctx.arc(b.x, b.y, r, 0, 2*Math.PI, false);
     ctx.fill();
 }
 
-function dot(v){ // v -> vector2
-    ctx.beginPath();
-    ctx.arc(v.x, v.y, r, 0, 2*Math.PI, false);
-    ctx.fill();
-}
-
+//再描画(履歴行き来用)
 function repaint(){
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     const len = p_vec.length;
     for(let i = len-1; i >= cur; i--){
-        color(colors[i]);
+        const {c, r} = setting[i];
         const dots = p_vec[i].length;
-        if(dots == 1) dot(p_vec[i][dots-1]);
+        color(c);
+        if(dots == 1) line(p_vec[i][dots-1],p_vec[i][dots-1], r);
         else {
             for(let j = dots-1; j >= 1; j--){
-                line(p_vec[i][j-1], p_vec[i][j]);
+                line(p_vec[i][j-1], p_vec[i][j], r);
             }
         }
     }
 }
 
+//色変更
 function color(num){
     let col;
     switch(num){
@@ -138,15 +166,19 @@ function color(num){
         case 0:
             col = "teal";
             break;
+        case "d":
+            col = "white";
+            break;
     }
     ctx.fillStyle = col;
     ctx.strokeStyle = col;
 }
 
-window.addEventListener("resize", ()=>{ onResize(); });
-
+//リサイズ時処理
 function onResize(){
     canvas.width = wrapper.offsetWidth;
     canvas.height = wrapper.offsetHeight;
     repaint();
 }
+
+window.addEventListener("resize", ()=>{ onResize(); });
